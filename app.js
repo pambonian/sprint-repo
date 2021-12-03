159bde8a539a42e1875d682dba49a22d4db08833
pambonian <pambonian@gmail.com>
2021-12-03 13:31:44 -0500

    ejs update

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const {} = require("./models");
+const {
+    user,
+    tweet,
+} = require("./models");
 const app = express();
 const PORT = process.envPORT || 8000;
 const layouts = require('express-ejs-layouts');
 const db = require('./models');
 const { RowDescriptionMessage } = require('pg-protocol/dist/messages');
+const user = require('./models/user');
+
 
 app.set('view engine', 'ejs');
 app.use(layouts);
@@ -14,6 +19,37 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.json({ message: 'Hey! A Functioning App Lives!'});
+});
+
+
+app.get('/users', function(req, res) {
+    user.findAll()
+    .then(function(userList) {
+        console.log('FOUND ALL USERS', userList);
+        res.render('users', { user: userList })
+    })
+    .catch(function(err) {
+        console.log('error!', err);
+        res.json({ message: 'Error loading users' });
+    });
+});
+
+app.get('/tweets', function(req, res) {
+    user.findAll()
+    .then(function(userList) {
+        console.log('FOUND ALL TWEETS', tweetList);
+        res.render('users', { user: tweetList })
+    })
+    .catch(function(err) {
+        console.log('error!', err);
+        res.json({ message: 'Error loading tweets' });
+    });
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is running on PORT', PORT);
 });
 
 // db.user.create({
